4ece3b182d32bcfbfa76212335be9f3d2834d86b
Jeroen Carolus <j.carolus@peoplesplayground.nl>
2014-10-12 09:14:58 +0200

    Italian language. Asset help files and values by Paolo Straffi

--- src/jwtc/android/chess/HtmlActivity.java
+++ src/jwtc/android/chess/HtmlActivity.java
@@ -1,5 +1,7 @@
 package jwtc.android.chess;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,36 +11,42 @@ import android.webkit.WebView;
 public class HtmlActivity extends Activity {
 
 	public static String HELP_MODE = "HELP_MODE";
-	
+
 	private WebView _webview;
 	private String _lang;
-	
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
-        setContentView(R.layout.help);
 
-        _lang = "en";
-        _webview = (WebView)findViewById(R.id.WebViewHelp);
-        
-    }
-    
-    @Override
-    protected void onResume() {
-        super.onResume();
-        
-        final Intent intent = getIntent();
-        //final Uri uri = intent.getData();
-        
-        Bundle extras =intent.getExtras(); 
-        if(extras != null){
-        	String s = extras.getString(HELP_MODE);
-        
-        	_webview.loadUrl("file:///android_asset/" + s + "-" + _lang + ".html");
-        }
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(R.layout.help);
+
+		// html assets localization
+		if (Locale.getDefault().getLanguage().equals("it")) {
+			_lang = "it";
+		} else {
+			_lang = "en";
+		}
+		_webview = (WebView) findViewById(R.id.WebViewHelp);
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		final Intent intent = getIntent();
+		// final Uri uri = intent.getData();
+
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+			String s = extras.getString(HELP_MODE);
+
+			_webview.loadUrl("file:///android_asset/" + s + "-" + _lang
+					+ ".html");
+		}
+	}
 }
