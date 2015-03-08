1398db6229f2fe933134d086149e0742ca2c0c4d
Jeroen Carolus <j.carolus@peoplesplayground.nl>
2015-03-08 13:58:38 +0100

    Compile native jni with latest ndk

---
+++ native/project/obj/local/armeabi/objs/chess-jni/Pos.o.d
@@ -0,0 +1,105 @@
+obj/local/armeabi/objs/chess-jni/Pos.o: jni/Pos.cpp jni/Pos.h \
+ jni/common.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/jcarolus/development/android-ndk-r10d/sources/cxx-stl/system/include/cstddef \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/math.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/time.h
+
+jni/Pos.h:
+
+jni/common.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/jcarolus/development/android-ndk-r10d/sources/cxx-stl/system/include/cstddef:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/math.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/Users/jcarolus/development/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/time.h:
