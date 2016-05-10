b8d7403f7311a4312c7ad3dc35b53228f8f46bde
Tim Tennyson <timmyt123@hotmail.com>
2016-05-10 09:43:32 -0700

    updated viewing games selection in ICS

--- app/src/main/java/jwtc/android/chess/ics/ICSClient.java
+++ app/src/main/java/jwtc/android/chess/ics/ICSClient.java
@@ -282,7 +282,8 @@ public class ICSClient extends MyBaseActivity implements OnItemClickListener {
 
 
         _adapterGames = new AlternatingRowColorAdapter(ICSClient.this, _mapGames, R.layout.ics_game_row,
-                new String[]{"text_name1", "text_name2", "text_rating1", "text_rating2"}, new int[]{R.id.text_name1, R.id.text_name2, R.id.text_rating1, R.id.text_rating2});
+                new String[]{"nr", "text_type", "text_name1", "text_name2", "text_rating1", "text_rating2", "text_time1", "text_time2"},
+                new int[]{R.id.nr, R.id.text_type, R.id.text_name1, R.id.text_name2, R.id.text_rating1, R.id.text_rating2, R.id.text_time1, R.id.text_time2});
 
         _listGames = (ListView) findViewById(R.id.ICSGames);
         _listGames.setAdapter(_adapterGames);
