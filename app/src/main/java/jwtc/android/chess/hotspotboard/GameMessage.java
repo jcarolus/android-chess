e9739ec3a2e49e2755dd7fbf59f3df53bf8f689d
Mohammed-Saalih <148184161+Mohammed-Saalih@users.noreply.github.com>
2025-07-06 22:30:58 +0530

    Working Draw/Resign
    When players disconnect they are considered lost . everything as usual .

--- app/src/main/java/jwtc/android/chess/hotspotboard/GameMessage.java
+++ app/src/main/java/jwtc/android/chess/hotspotboard/GameMessage.java
@@ -4,29 +4,40 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class GameMessage {
-    public String FEN;
-    public String white;
-    public String black;
+    public static final int TYPE_MOVE = 0;
+    public static final int TYPE_RESIGN = 1;
+    public static final int TYPE_DRAW_OFFER = 2;
+    public static final int TYPE_DRAW_ACCEPT = 3;
+    public static final int TYPE_DRAW_DECLINE = 4;
+    public static final int TYPE_NEW_GAME = 5;
 
-    public GameMessage(String FEN, String white, String black) {
+    public int type;
+    public String FEN;
+    public String playerName;
+
+    public GameMessage(int type, String FEN, String playerName) {
+        this.type = type;
         this.FEN = FEN;
-        this.white = white;
-        this.black = black;
+        this.playerName = playerName;
+    }
+
+    public GameMessage(String FEN, String playerName) {
+        this(TYPE_MOVE, FEN, playerName);
     }
 
     public static GameMessage fromJson(String jsonString) throws JSONException {
         JSONObject json = new JSONObject(jsonString);
+        int type = json.optInt("type", TYPE_MOVE);
         String FEN = json.getString("FEN");
-        String white = json.getString("white");
-        String black = json.getString("black");
-        return new GameMessage(FEN, white, black);
+        String playerName = json.optString("playerName", "");
+        return new GameMessage(type, FEN, playerName);
     }
 
     public String toJsonString() throws JSONException {
         JSONObject json = new JSONObject();
+        json.put("type", type);
         json.put("FEN", FEN);
-        json.put("white", white);
-        json.put("black", black);
+        json.put("playerName", playerName);
 
         return json.toString();
     }
