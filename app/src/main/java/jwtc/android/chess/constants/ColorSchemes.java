10e883ab1fbc276bd0312658299d8f73b9743287
Jeroen Carolus <>
2025-10-31 15:23:41 +0100

    WIP test with colors

--- app/src/main/java/jwtc/android/chess/constants/ColorSchemes.java
+++ app/src/main/java/jwtc/android/chess/constants/ColorSchemes.java
@@ -10,59 +10,50 @@ public class ColorSchemes {
     public static int selectedPattern = 0;
     
     static {
-        // yellow
         colorScheme[0][0] = 0xffdeac5d;
-        colorScheme[0][1] = 0xfff9e3c0;
-        colorScheme[0][2] = 0xfff3ed4b;
-        colorScheme[0][3] = 0xcc000000;
+        colorScheme[0][1] = 0xffE9D3B0;
+        colorScheme[0][2] = 0xff8A5A0C;
+        colorScheme[0][3] = 0xff8A5A0C;
 
-        // blue
         colorScheme[1][0] = 0xff6e98da;
         colorScheme[1][1] = 0xffa8e8ff;
         colorScheme[1][2] = 0xffcdedf7;
-        colorScheme[1][3] = 0xcc000000;
+        colorScheme[1][3] = 0xffcccccc;
 
-        // green - ,
         colorScheme[2][0] = 0xff769655;
         colorScheme[2][1] = 0xffeeeed2;
         colorScheme[2][2] = 0xff98e5ab;
-        colorScheme[2][3] = 0xcc000000;
+        colorScheme[2][3] = 0xffcccccc;
 
-        // grey
         colorScheme[3][0] = 0xffc0c0c0;
         colorScheme[3][1] = 0xffffffff;
         colorScheme[3][2] = 0xfff3ed4b;
-        colorScheme[3][3] = 0xcc000000;
+        colorScheme[3][3] = 0xffcccccc;
 
-        // brown
         colorScheme[4][0] = 0xffab6e41;
         colorScheme[4][1] = 0xffd6a47e;
         colorScheme[4][2] = 0xfff3ed4b;
-        colorScheme[4][3] = 0xcc000000;
+        colorScheme[4][3] = 0xffcccccc;
 
-        // red
         colorScheme[5][0] = 0xfffc4e4e;
         colorScheme[5][1] = 0xfffca9a9;
         colorScheme[5][2] = 0xfff3ed4b;
-        colorScheme[5][3] = 0xcc000000;
+        colorScheme[5][3] = 0xffcccccc;
 
-        // orange
         colorScheme[6][0] = 0xfffc9432;
         colorScheme[6][1] = 0xfffab778;
         colorScheme[6][2] = 0xfff3ed4b;
-        colorScheme[6][3] = 0xcc000000;
+        colorScheme[6][3] = 0xffcccccc;
 
-        // pink
         colorScheme[7][0] = 0xfff55fe3;
         colorScheme[7][1] = 0xfffc9ff1;
         colorScheme[7][2] = 0xfff3ed4b;
-        colorScheme[7][3] = 0xcc000000;
+        colorScheme[7][3] = 0xffcccccc;
 
-        // purple
         colorScheme[8][0] = 0xff805ad5;
         colorScheme[8][1] = 0xffac8eed;
         colorScheme[8][2] = 0xfffcd2f7;
-        colorScheme[8][3] = 0xcc000000;
+        colorScheme[8][3] = 0xffcccccc;
     }
 
     public static int getLight() {
