d71cda658b1c5226f2608d1620486939d6efc114
Jeroen Carolus <>
2026-02-07 13:07:43 +0100

    WIP material toggle group for game settings

--- app/src/main/java/jwtc/android/chess/play/GameSettingsDialog.java
+++ app/src/main/java/jwtc/android/chess/play/GameSettingsDialog.java
@@ -7,11 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
+import com.google.android.material.button.MaterialButtonToggleGroup;
 import jwtc.android.chess.R;
 import jwtc.android.chess.engine.EngineApi;
 import jwtc.android.chess.helpers.ResultDialog;
@@ -31,12 +31,9 @@ public class GameSettingsDialog extends ResultDialog {
         final int levelPly = prefs.getInt("levelPly", 2);
         final boolean quiescentSearchOn = prefs.getBoolean("quiescentSearchOn", true);
 
-        final RadioButton radioAndroid = findViewById(R.id.radioAndroid);
-        final RadioButton radioHuman = findViewById(R.id.radioHuman);
-        final RadioButton radioWhite = findViewById(R.id.radioWhite);
-        final RadioButton radioBlack = findViewById(R.id.radioBlack);
-        final RadioButton radioTime = findViewById(R.id.RadioOptionsTime);
-        final RadioButton radioPly = findViewById(R.id.RadioOptionsPly);
+        final MaterialButtonToggleGroup toggleOpponent = findViewById(R.id.ToggleOpponentGroup);
+        final MaterialButtonToggleGroup toggleColor = findViewById(R.id.ToggleColorGroup);
+        final MaterialButtonToggleGroup toggleLevelMode = findViewById(R.id.ToggleLevelModeGroup);
         final Spinner spinnerLevelTime = findViewById(R.id.SpinnerOptionsLevelTime);
         final Spinner spinnerLevelPly = findViewById(R.id.SpinnerOptionsLevelPly);
         final ToggleButton toggleQuiescent = findViewById(R.id.ToggleQuiescent);
@@ -53,29 +50,13 @@ public class GameSettingsDialog extends ResultDialog {
 
         toggleQuiescent.setChecked(quiescentSearchOn);
 
-        radioAndroid.setChecked(vsCPU);
-        radioHuman.setChecked(!vsCPU);
+        toggleOpponent.check(vsCPU ? R.id.radioAndroid : R.id.radioHuman);
 
-        radioWhite.setChecked(playAsWhite);
-        radioBlack.setChecked(!playAsWhite);
+        toggleColor.check(playAsWhite ? R.id.radioWhite : R.id.radioBlack);
 
-        radioTime.setChecked(levelMode == EngineApi.LEVEL_TIME);
-        radioPly.setChecked(levelMode == EngineApi.LEVEL_PLY);
-
-        // radio group
-        radioTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                radioPly.setChecked(!radioTime.isChecked());
-            }
-        });
-
-        radioPly.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                radioTime.setChecked(!radioPly.isChecked());
-            }
-        });
+        toggleLevelMode.check(levelMode == EngineApi.LEVEL_TIME
+                ? R.id.RadioOptionsTime
+                : R.id.RadioOptionsPly);
 
         spinnerLevelTime.setSelection(levelTime - 1);
         spinnerLevelPly.setSelection(levelPly - 1);
@@ -87,10 +68,12 @@ public class GameSettingsDialog extends ResultDialog {
 
                 SharedPreferences.Editor editor = prefs.edit();
 
-                editor.putBoolean("opponent", radioAndroid.isChecked());
-                editor.putBoolean("myTurn", radioWhite.isChecked());
+                editor.putBoolean("opponent", toggleOpponent.getCheckedButtonId() == R.id.radioAndroid);
+                editor.putBoolean("myTurn", toggleColor.getCheckedButtonId() == R.id.radioWhite);
 
-                editor.putInt("levelMode", radioTime.isChecked() ? EngineApi.LEVEL_TIME : EngineApi.LEVEL_PLY);
+                editor.putInt("levelMode", toggleLevelMode.getCheckedButtonId() == R.id.RadioOptionsTime
+                        ? EngineApi.LEVEL_TIME
+                        : EngineApi.LEVEL_PLY);
                 editor.putInt("level", spinnerLevelTime.getSelectedItemPosition() + 1);
                 editor.putInt("levelPly", spinnerLevelPly.getSelectedItemPosition() + 1);
 
